953067459655b12f56da978d9b712f9c788f6277
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-12 13:58:09 -0600

    Final calculation for the accuracy of linear model

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ df[["Status"]] = df[["Status"]].replace("Developing", 0)
 # print(data.head())
 
 # Little correlation: "Alcohol", "under-five deaths" that can be considered to add to the model
-data = df[["Life expectancy","Adult Mortality", "infant deaths", "Status", "HIV/AIDS", "GDP",
+data = df[["Life expectancy","Adult Mortality", "infant deaths", "Status", "HIV/AIDS", "GDP", "Polio", "Diphtheria", "percentage expenditure",
          "Income composition of resources","Schooling"]].copy()
 # print(data.head())
 predict = "Life expectancy"
@@ -22,7 +22,7 @@ predict = "Life expectancy"
 # Print out the list of missing values in each feature
 # print(data.isnull().sum(), "\n")
 
-# Print out the percentage of missing values in each feature. I will impute substitue values
+# Print out the percentage of missing values in each feature.
 # print(data.isnull().sum() / len(data))
 # Since there isn't a big percentage of missing values for each feature, I will impute missing values with means and medians
 life_expectance_mean = data[predict].mean()
@@ -31,6 +31,9 @@ data["Adult Mortality"].fillna(data["Adult Mortality"].mean(), inplace=True)
 data["GDP"].fillna(data["GDP"].median(), inplace=True)
 data["Income composition of resources"].fillna(data["Income composition of resources"].median(), inplace=True)
 data["Schooling"].fillna(data["Schooling"].median(), inplace=True)
+data["Polio"].fillna(data["Polio"].median(), inplace=True)
+data["Diphtheria"].fillna(data["Diphtheria"].mean(), inplace=True)
+data["percentage expenditure"].fillna(data["percentage expenditure"].mean(), inplace=True)
 # print(data.isnull().sum(), "\n")
 
 x = np.array(data.drop(columns = [predict]))
@@ -42,35 +45,36 @@ linear.fit(x_train, y_train)
 
 
 
-best = 0
-for _ in range(30):
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
-    linear = linear_model.LinearRegression();
-
-    linear.fit(x_train, y_train)
-
-    # Time 100 to get the acurracy percentage
-    acc = linear.score(x_test, y_test)
-    print("Accuracy Percentage: ", format(acc, "%"));
-
-    if acc > best:
-        best = acc
-        with open("life-expectancy-model.pickle", "wb") as f:
-            pickle.dump(linear, f)
-
-
-
-
-
-# acc = linear.score(x_test, y_test)
-# print("Accuracy Percentage: ", format(acc, "%"));
+# best = 0
+# for _ in range(30):
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+#     linear = linear_model.LinearRegression();
+#
+#     linear.fit(x_train, y_train)
+#
+#     # Time 100 to get the acurracy percentage
+#     acc = linear.score(x_test, y_test)
+#     print("Accuracy Percentage: ", format(acc, "%"));
+#
+#     if acc > best:
+#         best = acc
+#         with open("life-expectancy-model.pickle", "wb") as f:
+#             pickle.dump(linear, f)
 
 
 
 
-p = "GDP"
-style.use("ggplot")
-pyplot.scatter(data[p], data["Life expectancy"])
-pyplot.xlabel(p)
-pyplot.ylabel("Life expectancy")
-pyplot.show()
+pickle_in = open("life-expectancy-model.pickle", "rb")
+linear = pickle.load(pickle_in)
+acc = linear.score(x_test, y_test)
+print("Accuracy Percentage: ", format(acc, "%"));
+
+
+
+
+# p = "GDP"
+# style.use("ggplot")
+# pyplot.scatter(data[p], data["Life expectancy"])
+# pyplot.xlabel(p)
+# pyplot.ylabel("Life expectancy")
+# pyplot.show()
